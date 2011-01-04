b8dddb614a6b50a6eb684f5bafd13d584f825f41
neiled <neil@plasticwater.com>
2011-01-04 21:34:10 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brollyreminder::Application.config.secret_token = '9e479c195837cdfe4cc100a07a24cb4ad6b468146df3833e704c661bf1b83491c34dfb27976d7a8e0737ace981e4ad62bfecb4795dc24895b37d60b0006067f2'
